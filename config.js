67e51ac281abf849bba973d3f29a1c9980537578
SizzyFN <thatimmortalguy0047@gmail.com>
2024-05-26 14:44:02 +0100

    Moyo md

--- config.js
+++ config.js
@@ -8,14 +8,14 @@ import axios from 'axios'
 global.owner = [
   ['918360234087', 'WHITE', true], 
   ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['2348138296400', '𝐄𝐕𝐄𝐑𝐘𝐁𝐎𝐃𝐘 𝐇𝐀𝐓𝐄𝐒 𝐌𝐎𝐘𝐎', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
   ['233508488192', 'Abraham Dwamena3', true]
 ] //Number of owners
 
-global.mods = ['923184070915', '94789481495', '923184474176', '233531910067', '233508488192'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
+global.mods = ['2348138296400', '94789481495', '923184474176', '233531910067', '233508488192'] 
+global.prems = ['2348138296400', '233208179431', '94789481495', '918360234087', '233508488192']
 global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = '𝐌𝐎𝐘𝐎 𝐌𝐃'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = '𝐄𝐕𝐄𝐑𝐘𝐁𝐎𝐃𝐘 𝐇𝐀𝐓𝐄𝐒 𝐌𝐎𝐘𝐎' 
+global.author = '@𝐄𝐕𝐄𝐑𝐘𝐁𝐎𝐃𝐘 𝐇𝐀𝐓𝐄𝐒 𝐌𝐎𝐘𝐎' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
